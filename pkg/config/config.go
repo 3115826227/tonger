b6ef1d212da51f4917b87ea0053aad4f8e779d2a
mayu <mayu@mayudeMacBook-Pro.local>
2021-03-09 20:30:03 +0800

    移动配置文件的位置

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -17,10 +17,10 @@ var (
 )
 
 func ReadConfig() {
-	viper.SetConfigFile("./etc/config.yaml") // 指定配置文件路径
+	viper.SetConfigFile("./res/config.yaml") // 指定配置文件路径
 	viper.SetConfigName("config")            // 配置文件名称(无扩展名)
 	viper.SetConfigType("yaml")              // 如果配置文件的名称中没有扩展名，则需要配置此项
-	viper.AddConfigPath("./etc/")            // 查找配置文件所在的路径
+	viper.AddConfigPath("./res/")            // 查找配置文件所在的路径
 	err := viper.ReadInConfig()              // 查找并读取配置文件
 	if err != nil {                          // 处理读取配置文件的错误
 		panic(fmt.Errorf("Fatal error config file: %s \n", err))
